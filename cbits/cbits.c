0f0daa6737897ce2fab3d1b7246f07cff095b3bd
Herbert Valerio Riedel <hvr@gnu.org>
2018-01-17 21:31:07 +0100

    trailing whitespace removal

--- cbits/cbits.c
+++ cbits/cbits.c
@@ -1,23 +1,23 @@
 /*
  * Copyright (c) 2017, Herbert Valerio Riedel
- *  
+ *
  * All rights reserved.
- *  
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- *  
+ *
  *     * Redistributions of source code must retain the above copyright
  *       notice, this list of conditions and the following disclaimer.
- *  
+ *
  *     * Redistributions in binary form must reproduce the above
  *       copyright notice, this list of conditions and the following
  *       disclaimer in the documentation and/or other materials provided
  *       with the distribution.
- *  
+ *
  *     * Neither the name of Herbert Valerio Riedel nor the names of other
  *       contributors may be used to endorse or promote products derived
  *       from this software without specific prior written permission.
- *  
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -50,9 +50,9 @@ hs_text_short_length(const uint8_t buf[], const size_t n)
 /* Validate UTF8 encoding
 
  7 bits | 0xxxxxxx
-11 bits | 110yyyyx  10xxxxxx 
-16 bits | 1110yyyy  10yxxxxx  10xxxxxx 
-21 bits | 11110yyy  10yyxxxx  10xxxxxx  10xxxxxx 
+11 bits | 110yyyyx  10xxxxxx
+16 bits | 1110yyyy  10yxxxxx  10xxxxxx
+21 bits | 11110yyy  10yyxxxx  10xxxxxx  10xxxxxx
 
 Valid code-points:
 
