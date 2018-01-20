c7ba4dfb8f40c4c4bb9b65098c3f99a6fe64754f
Herbert Valerio Riedel <hvr@gnu.org>
2018-01-20 15:49:35 +0100

    Implement more common verbs

--- src/Data/Text/Short.hs
+++ src/Data/Text/Short.hs
@@ -13,6 +13,11 @@ module Data.Text.Short
       ShortText
 
       -- * Basic operations
+    , pack
+    , unpack
+    , append
+    , concat
+    , empty
     , null
     , length
     , isAscii
@@ -41,6 +46,15 @@ module Data.Text.Short
     , spanEnd
     , breakEnd
 
+    , take
+    , takeEnd
+    , drop
+    , dropEnd
+    , takeWhile
+    , takeWhileEnd
+    , dropWhile
+    , dropWhileEnd
+
       -- * Conversions
       -- ** 'Char'
     , singleton
@@ -64,9 +78,10 @@ module Data.Text.Short
 
     ) where
 
+import           Data.Semigroup
 import           Data.Text.Short.Internal
 import           Prelude                  (Bool (..), Char, Int, Maybe (..),
-                                           not, (.))
+                                           fst, not, snd, (.))
 
 -- | \(\mathcal{O}(n)\) Variant of 'span' with negated predicate.
 --
@@ -99,3 +114,91 @@ any :: (Char -> Bool) -> ShortText -> Bool
 any p st = case find p st of
              Nothing -> False
              Just _  -> True
+
+-- | \(\mathcal{O}(n)\) Concatenate two 'ShortText's
+--
+-- This is a type-specialised alias of '<>'.
+--
+-- @since TBD
+append :: ShortText -> ShortText -> ShortText
+append = (<>)
+
+-- | \(\mathcal{O}(n)\) Concatenate list of 'ShortText's
+--
+-- This is a type-specialised alias of 'mconcat'.
+--
+-- @since TBD
+concat :: [ShortText] -> ShortText
+concat = mconcat
+
+-- | \(\mathcal{O}(0)\) The /empty/ 'ShortText'.
+--
+-- This is a type-specialised alias of 'mempty'.
+--
+-- @since TBD
+empty :: ShortText
+empty = mempty
+
+-- | \(\mathcal{O}(n)\) Construct a 'ShortText' from a list of 'Char's.
+--
+-- This is an alias for 'fromString'.
+--
+-- @since TBD
+pack :: [Char] -> ShortText
+pack = fromString
+
+-- | \(\mathcal{O}(n)\) Convert 'ShortText' into a list of 'Char's.
+--
+-- This is an alias for 'toString'.
+--
+-- @since TBD
+unpack :: ShortText -> [Char]
+unpack = toString
+
+-- | \(\mathcal{O}(n)\) Take prefix of given length or return whole 'ShortText' if too short.
+--
+-- @since TBD
+take :: Int -> ShortText -> ShortText
+take n = fst . splitAt n
+
+-- | \(\mathcal{O}(n)\) Take suffix of given length or return whole 'ShortText' if too short.
+--
+-- @since TBD
+takeEnd :: Int -> ShortText -> ShortText
+takeEnd n = snd . splitAtEnd n
+
+-- | \(\mathcal{O}(n)\) Take remove prefix of given length from 'ShortText' or return 'empty' 'ShortText' if too short.
+--
+-- @since TBD
+drop :: Int -> ShortText -> ShortText
+drop n = snd . splitAt n
+
+-- | \(\mathcal{O}(n)\) Take remove suffix of given length from 'ShortText' or return 'empty' 'ShortText' if too short.
+--
+-- @since TBD
+dropEnd :: Int -> ShortText -> ShortText
+dropEnd n = fst . splitAtEnd n
+
+-- | \(\mathcal{O}(n)\) Take longest prefix satisfying given predicate.
+--
+-- @since TBD
+takeWhile :: (Char -> Bool) -> ShortText -> ShortText
+takeWhile p = fst . span p
+
+-- | \(\mathcal{O}(n)\) Take longest suffix satisfying given predicate.
+--
+-- @since TBD
+takeWhileEnd :: (Char -> Bool) -> ShortText -> ShortText
+takeWhileEnd p = snd . spanEnd p
+
+-- | \(\mathcal{O}(n)\) Remove longest prefix satisfying given predicate.
+--
+-- @since TBD
+dropWhile :: (Char -> Bool) -> ShortText -> ShortText
+dropWhile p = snd . span p
+
+-- | \(\mathcal{O}(n)\) Remove longest suffix satisfying given predicate.
+--
+-- @since TBD
+dropWhileEnd :: (Char -> Bool) -> ShortText -> ShortText
+dropWhileEnd p = fst . spanEnd p
